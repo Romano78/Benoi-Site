0621f3984322751d7a6af2de47d86de801d307c0
Romano78 <rom.penaruiz@gmail.com>
2020-11-05 16:13:57 -0500

    icon removed for test

--- gatsby-config.js
+++ gatsby-config.js
@@ -88,8 +88,6 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#000000`,
         display: `minimal-ui`,
-        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
-        include_favicon: true,
       },
     },
     `gatsby-plugin-eslint`,
